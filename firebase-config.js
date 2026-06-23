f04089fbf6da2a746853f9f20058f65b4e883a7a
radarmaprf <y697b8yzz2@privaterelay.appleid.com>
2026-06-23 14:58:38 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCksMAKAI1U2gXRHefO0vAZYbY4Y6ES1Sg",
+    authDomain: "keys-c7467.firebaseapp.com",
+    databaseURL: "https://keys-c7467-default-rtdb.firebaseio.com",
+    projectId: "keys-c7467",
+    storageBucket: "keys-c7467.firebasestorage.app",
+    messagingSenderId: "573161424340",
+    appId: "1:573161424340:web:6fe203312d2a18079f0241"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const usersRef = db.ref('users');
+const sessionsRef = db.ref('sessions');
+const gamesRef = db.ref('games');
